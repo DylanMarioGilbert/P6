a73f19eec44382a228911c5b9eae29f56ef5ec6d
Dylan Gilbert <39006574@parisnanterre.fr>
2023-11-16 03:35:21 +0100

    Step 2

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -9,6 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
             setupFilters(projects); // Configuration des boutons de filtre
         })
         .catch(error => console.error('Erreur : ', error));
+        // ... (ton code existant)
+
+
 });
 
 // Filtrage des projets par catégorie
@@ -111,9 +114,27 @@ function setupFilters(projects) {
 
                 filterContainer.appendChild(button);
             });
-        })
+            // Ajoute la condition pour gérer l'affichage des boutons
+            if (checkUserLoggedIn()) {
+
+                // Masquer les boutons individuels
+            const buttons = document.querySelectorAll('#category-filters button');
+            buttons.forEach(button => {
+            button.style.display = 'none';
+            });
+            
+            }
+            // Ajouter une marge ou un espace supplémentaire entre les projets et les images de la galerie
+            const gallery = document.querySelector('.gallery');
+            gallery.style.marginTop = '20px'; // Vous pouvez ajuster la valeur selon vos besoins
+            })
+            
+            
+            
         .catch(error => console.error('Erreur lors de la récupération des catégories : ', error));
 }
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // Récupérer l'élément "login-link" pour y attacher un gestionnaire d'événement
     const loginLink = document.getElementById('login-link');
@@ -124,4 +145,64 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Vérifier si l'utilisateur est connecté (utilisez votre propre logique)
+    const isLoggedIn = checkUserLoggedIn();
+    console.log('User is logged in:', isLoggedIn);
+
+    // Configurer la page en fonction de l'état de connexion
+    configurePage(isLoggedIn);
+});
+
+function checkUserLoggedIn() {
+    // Implémentez votre propre logique pour vérifier si l'utilisateur est connecté
+    // Retournez true si connecté, false sinon
+    const token = localStorage.getItem('token');
+    return token !== null;
+}
+
+function configurePage(isLoggedIn) {
+    if (isLoggedIn) {
+        // Si l'utilisateur est connecté, mettre à jour la page
+        console.log('User is logged in. Updating page...');
+        updatePageForLoggedInUser();
+    } else {
+        // Si l'utilisateur n'est pas connecté, exécuter la configuration initiale
+        console.log('User is not logged in. Setting up initial page...');
+        setupInitialPage();
+    }
+}
+
+
+function updatePageForLoggedInUser() {
+    console.log('Updating page for logged-in user...');
+    // Cacher le bouton Login et afficher le bouton Logout
+    const loginLink = document.getElementById('login-link');
+    const logoutLink = document.getElementById('logout-link');
+    if (loginLink && logoutLink) {
+        loginLink.style.display = 'none';
+        logoutLink.style.display = 'block';
+    }
+
+    // Afficher l'icône Modifier dans le menu de navigation
+    const editModeIcon = document.getElementById('edit-mode-icon');
+    if (editModeIcon) {
+        editModeIcon.style.display = 'block';
+    } 
+    
+
+ 
+    // Modifier l'en-tête pour le mode édition avec un fond noir
+    const siteHeader = document.getElementById('site-header');
+    if (siteHeader) {
+        siteHeader.style.backgroundColor = 'black';
+    }
+
+    // Changer le texte du titre si nécessaire
+    const siteTitle = document.getElementById('site-title');
+    if (siteTitle) {
+        siteTitle.textContent = 'Mode édition';
+        siteTitle.color = 'White';
+    }
+}
 
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -7,8 +7,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const passwordLabel = document.querySelector('label[for="password"]');
     const passwordInput = document.getElementById('password');
 
-    loginTitle.style.marginBottom = '30px'; // Ajuster la marge inférieure du titre
-
+    if (loginTitle) {
+        loginTitle.style.marginBottom = '30px';
+    }
     emailInput.style.width = '250px'; // Ajuster la largeur de l'input email
     passwordInput.style.width = '250px'; // Ajuster la largeur de l'input password
 
@@ -53,6 +54,109 @@ document.addEventListener('DOMContentLoaded', function() {
     submitButton.style.cursor = 'pointer'; // Changement de curseur au survol
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.querySelector('form');
+    const errorMessage = document.getElementById('error-message');
 
-  
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        // Envoi de la requête POST à l'endpoint d'authentification
+        fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Identifiants invalides');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Traitement de la réponse
+            if (data.token) {
+                // Stocker le token dans le stockage local (localStorage)
+                localStorage.setItem('token', data.token);
+
+         
+                // Authentification réussie, rediriger vers la page d'accueil
+                window.location.href = 'index.html';
+
+
+            } else {
+                // Authentification échouée, afficher un message d'erreur par exemple
+                console.error('Identifiants invalides');
+            }
+        })
+        .catch(error => {
+            // Afficher le message d'erreur en cas d'erreur de la requête
+            errorMessage.textContent = 'Erreur lors de la requête : ' + error.message;
+        });
+        
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            // L'utilisateur est connecté
+        } else {
+            // L'utilisateur n'est pas connecté
+        }
+    });
+    // Récupérer l'élément "logout-link" pour y attacher un gestionnaire d'événement
+    const logoutLink = document.getElementById('logout-link');
+
+    // Lorsque l'utilisateur clique sur "logout-link", effectuer la déconnexion
+    logoutLink.addEventListener('click', function() {
+        // Supprimer le token du stockage local
+        localStorage.removeItem('token');
+
+        // Mettre à jour la page pour l'utilisateur déconnecté
+        updatePageForLoggedOutUser();
+    });
+    function updatePageForLoggedOutUser() {
+        // Afficher le bouton Login et masquer le bouton Logout
+        const loginLink = document.getElementById('login-link');
+        const logoutLink = document.getElementById('logout-link');
+        if (loginLink && logoutLink) {
+            loginLink.style.display = 'block';
+            logoutLink.style.display = 'none';
+        }
+    
+        // Masquer l'icône Modifier dans le menu de navigation
+        const editModeIcon = document.getElementById('edit-mode-icon');
+        if (editModeIcon) {
+            editModeIcon.style.display = 'none';
+        }
+    
+        // Afficher les boutons de filtre de catégories
+        const categoryFilters = document.getElementById('category-filters');
+        if (categoryFilters) {
+        categoryFilters.style.display = 'flex';
+        categoryFilters.style.justifyContent = 'center';
+        }
+        // Si l'utilisateur n'est pas connecté, rétablir l'affichage des boutons
+        const buttons = document.querySelectorAll('#category-filters button');
+        buttons.forEach(button => {
+            button.style.display = 'block';
+        });
+    
+        // Restaurer l'en-tête à son état initial
+        const siteHeader = document.getElementById('site-header');
+        if (siteHeader) {
+            siteHeader.style.backgroundColor = ''; // Remettre à la couleur d'origine
+        }
+    
+        // Restaurer le texte du titre si nécessaire
+        const siteTitle = document.getElementById('site-title');
+        if (siteTitle) {
+            siteTitle.textContent = 'Sophie Bluel Architecte d\'intérieur';
+        }
+    }
+    
+});
 
